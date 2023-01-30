3a9ebc17d5e5b72f5b101a7628db33407b767c35
Andrew Kang <akang312@gmail.com>
2023-01-30 19:36:27 +0000

    Completed javascript-operators-and-expressions exercise

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,44 @@
+var width = 7;
+var height = 3;
+var area = width * height;
+
+console.log('value of area:', area);
+console.log('typeof area:', typeof area);
+
+var bill = 21.89;
+var payment = 30;
+var change = payment - bill;
+
+console.log('value of change:', change);
+console.log('typeof change:', typeof change);
+
+var quizzes = 70;
+var midterm = 90;
+var final = 80;
+var grade = (quizzes + midterm + final) / 3;
+
+console.log('value of grade:', grade);
+console.log('typeof grade:', typeof grade);
+
+var firstName = 'Andrew ';
+var lastName = 'Kang';
+var fullName = firstName + lastName;
+
+console.log('value of fullName:', fullName);
+console.log('typeof fullName:', typeof fullName);
+
+var pH = 43;
+var isAcidic = pH > 7;
+console.log('value of isAcidic:', isAcidic);
+console.log('typeof is Acidic:', typeof isAcidic);
+
+var headCount = 12;
+var isSparta = headCount === 300;
+
+console.log('value of isSparta', isSparta);
+console.log('typeof isSparta', typeof isSparta);
+
+var motto = fullName + ' is the GOAT';
+
+console.log('value of motto:', motto);
+console.log('typeof motto:', typeof motto);
