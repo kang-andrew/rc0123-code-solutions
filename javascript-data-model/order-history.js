5996af4468eb9074de2a252a7d3a64f50262e607
Andrew Kang <akang312@gmail.com>
2023-01-31 06:29:24 +0000

    Completed javascript-data-model exercise

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,48 @@
+var orderHistory = [
+  {
+    orderPlaced: 'August 4, 2020',
+    total: '34.00',
+    shipTo: 'JS Masher',
+    orderNumber: '114-3941689-8772232',
+    deliveryDate: 'August 8, 2020',
+    productPurchased: 'book',
+    title: 'JavaScript for impatient programmers',
+    author: 'Dr. Axel Rauschmayer',
+    returnWindow: 'September 7, 2020'
+  },
+  {
+    orderPlaced: 'July 20, 2020',
+    total: '44.53',
+    shipTo: 'JS Masher',
+    orderNumber: '114-3941689-8772232',
+    deliveryDate: 'July 20, 2020',
+    productPurchased: 'book',
+    title: 'The Timeless Way of Building',
+    author: 'Christopher Alexander',
+    returnWindow: 'August 19, 2020'
+  },
+  {
+    orderPlaced: 'July 4, 2020',
+    total: '17.22',
+    shipTo: 'JS Masher',
+    orderNumber: '114-2875557-9059409',
+    deliveryDate: 'July 7, 2020',
+    productPurchased: 'gamecube accessory',
+    title: undefined,
+    author: undefined,
+    returnWindow: 'August 5, 2020'
+  },
+  {
+    orderPlaced: 'July 3, 2020',
+    total: '138.93',
+    shipTo: 'JS Masher',
+    orderNumber: '113-2883177-2648248',
+    deliveryDate: 'July 5, 2020',
+    productPurchased: ['gamecube accessory', 'book'],
+    title: [undefined, 'The Art of Sql'],
+    author: [undefined, 'Stephane Faroult'],
+    returnWindow: 'August 4, 2020'
+  }
+];
+
+console.log(orderHistory);
