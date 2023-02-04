49725f0109e794616e84bf0d3e93b53c4ef45959
Andrew Kang <akang312@gmail.com>
2023-02-04 00:28:59 +0000

    Shortened compact code

--- array-manipulation-1/src/compact.js
+++ array-manipulation-1/src/compact.js
@@ -2,9 +2,7 @@
 function compact(array) {
   var compactedArray = [];
   for (var i = 0; i < array.length; i++) {
-    if (!!array[i] === false) {
-      continue;
-    } else if (!!array[i] === true) {
+    if (array[i]) {
       compactedArray.push(array[i]);
     }
   }
