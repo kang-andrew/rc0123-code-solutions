0e1a48270bc2bd9cffea68459531421ad287ec7a
Andrew Kang <akang312@gmail.com>
2023-02-06 18:40:42 +0000

    Revised dom-querying

--- dom-querying/main.js
+++ dom-querying/main.js
@@ -1,8 +1,17 @@
-console.log(document.querySelector('h1'));
-console.dir('h1');
-console.log(document.querySelector('#explanation'));
-console.dir('#explanation');
-console.log(document.querySelector('.hint'));
-console.dir('.hint');
-console.log(document.querySelectorAll('p'));
-console.log(document.querySelectorAll('.example-link'));
+var $heading = document.querySelector('h1');
+console.log('$heading:', $heading);
+console.dir($heading);
+
+var $explanation = document.querySelector('#explanation');
+console.log('$explanation:', $explanation);
+console.dir($explanation);
+
+var $hint = document.querySelector('.hint');
+console.log('$hint:', $hint);
+console.dir($hint);
+
+var $paragraph = document.querySelectorAll('p');
+console.log('$paragraph:', $paragraph);
+
+var $exampleLinks = document.querySelectorAll('.example-link');
+console.log('$exampleLinks:', $exampleLinks);
