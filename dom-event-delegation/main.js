191f66e92c3c8a85ec7df8213cfbe325233025aa
Andrew Kang <akang312@gmail.com>
2023-02-09 19:49:32 +0000

    Finished step 7

--- dom-event-delegation/main.js
+++ dom-event-delegation/main.js
@@ -4,7 +4,9 @@ $taskListClick.addEventListener('click', function (event) {
   console.log('event.target:', event.target);
   console.log('event.target.tagName:', event.target.tagName);
   if (event.target.matches('button')) {
-    console.log('closest:', event.target.closest('.task-list-item'));
+    var $closestElement = event.target.closest('.task-list-item');
+    console.log('closest:', $closestElement);
+    $closestElement.remove();
   }
 }
 );
